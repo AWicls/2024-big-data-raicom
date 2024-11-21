6fb1eb3e525e6cb811e2bc72ec7a00ce9c2c6f97
AWicls <89051064+AWicls@users.noreply.github.com>
2024-11-21 14:11:32 +0800

    connect_hive

--- practicespark/src/connect_hive.py
+++ practicespark/src/connect_hive.py
@@ -1,6 +1,25 @@
 from pyhive import hive
+from read_conf import read_yaml
 
 
-def connect_hive(conf):
-    hive.Connection()
-    return 0
+# 连接hive并返回curser对象
+def connect_hive_to_curser():
+    filepath = r'../config/config.yaml'
+    hiveconf = read_yaml(filepath).get('hive')
+    conn = hive.Connection(
+        host=hiveconf.get('host'),
+        port=hiveconf.get('port'),
+        username=hiveconf.get('username'),
+        database=hiveconf.get('database'),
+        auth=hiveconf.get('auth'),
+        configuration=hiveconf.get('configuration'))
+    return conn.cursor()
+
+
+# 连接测试
+if __name__ == '__main__':
+    curser = connect_hive_to_curser()
+    sql = 'show tables'
+    curser.execute(sql)
+    result = curser.fetchall()
+    print(result)
